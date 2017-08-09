1b66e5ad671caf93e01fe3e0cac8aaf89f601ad2
Michael Joseph <michaelpjoseph@gmail.com>
2017-08-08 20:52:57 -0400

    updated

--- beeper/beeper.ino
+++ beeper/beeper.ino
@@ -1,33 +1,17 @@
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the Uno and
-  Leonardo, it is attached to digital pin 13. If you're unsure what
-  pin the on-board LED is connected to on your Arduino model, check
-  the documentation at http://www.arduino.cc
-
-  This example code is in the public domain.
-
-  modified 8 May 2014
-  by Scott Fitzgerald
- */
-
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin 13 as an output.
-  pinMode(13, OUTPUT);
-   pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);  // primary beeper
+  pinMode(12, OUTPUT);  // optional secondary beeper
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  //digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-  analogWrite(13,100);
+  digitalWrite(13, HIGH);   // turn the pin on (HIGH is the voltage level)
   //digitalWrite(12, LOW);
-  delay(100);              // wait for a second
-  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);              // wait for a second
+  digitalWrite(13, LOW);    // turn the pin off by making the voltage LOW
    //digitalWrite(12, HIGH);
   delay(1000);              // wait for a second
 }
